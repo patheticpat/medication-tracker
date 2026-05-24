2622da78d91016b20479a101dc4a3a9dc9aabd73
Michael Kaiser <patheticpat@googlemail.com>
2026-05-24 12:23:04 +0200

    FE: make sort by urgency the default

--- web/src/stores/ui.ts
+++ web/src/stores/ui.ts
@@ -2,7 +2,7 @@ import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
 export const useUI = defineStore('ui', () => {
-  const sortOrder = ref<'alphabetical' | 'urgency'>('alphabetical')
+  const sortOrder = ref<'alphabetical' | 'urgency'>('urgency')
   const toggleSortOrder = () => {
     sortOrder.value = sortOrder.value === 'alphabetical' ? 'urgency' : 'alphabetical'
   }
