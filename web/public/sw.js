4e740e6a1078776d0e43e5d3e1957673d905e29f
Michael Kaiser <patheticpat@googlemail.com>
2026-05-29 20:21:09 +0200

    FE: onClick notification handler

--- web/public/sw.js
+++ web/public/sw.js
@@ -9,3 +9,8 @@ self.addEventListener('push', (event) => {
     }),
   )
 })
+
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close()
+  event.waitUntil(clients.openWindow('/'))
+})
