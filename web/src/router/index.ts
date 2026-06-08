12fc174861d530f7fcc2477b126be2820ffd603c
Michael Kaiser <patheticpat@googlemail.com>
2026-06-08 20:33:26 +0200

    FE: set scrollBehavior on router

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  scrollBehavior: (_to, _from, savedPosition) => (savedPosition ? savedPosition : { top: 0 }),
   routes: [
     { path: '/', name: 'home', component: () => import('@/views/HomeView.vue') },
     { path: '/callback', name: 'callback', component: () => import('@/views/CallbackView.vue') },
